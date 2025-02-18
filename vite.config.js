2aff2faeed53dfdf894504b3644e50f7cc135083
VedantSonavane <92395669+VedantSonavane@users.noreply.github.com>
2025-02-18 07:29:25 +0000

    Configured GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/', // This must match your GitHub repo name exactly
+  base: '/Portfolio/', // This must match your GitHub repo name exactly
 });
