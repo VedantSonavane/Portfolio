5567327cc9b2c941c228ca04b99553518f0665fa
VedantSonavane <92395669+VedantSonavane@users.noreply.github.com>
2025-02-20 07:32:00 +0000

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // This must match your GitHub repo name exactly
+  base: '/', // This must match your GitHub repo name exactly
 });
